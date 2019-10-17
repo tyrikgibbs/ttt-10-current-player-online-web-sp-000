c7846dc271c26cd24d92108822e831f9814f0d3e
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-17 16:48:28 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   counter = 1
   board.each do |turn|
-    if board[index] == "X" or "O"
+    if board[turn] == "X" or "O"
     counter +=1
     end
   end
